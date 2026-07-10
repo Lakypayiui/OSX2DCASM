93f03073a26e9c95aed81cab0cde211b167302cd
Ian <laicanciopablo@gmail.com>
2026-07-09 23:25:12 -0600

    feat: enhance process PID logging in main and simulation scene for better debugging

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import os
+import sys
 from multiprocessing import freeze_support, set_start_method
+import multiprocessing as mp
 from acosxm import ACOSXM
+if "main" in sys.modules or getattr(sys, 'frozen', False):
+    os.environ.setdefault('MAIN_PID', str(os.getpid()))
 
 if __name__ == "__main__":
     freeze_support()
-    set_start_method("spawn")
+    mp.set_start_method("spawn", force=True)
+    
+    print(f"=== PROCESO PRINCIPAL INICIADO PID = {os.getpid()} ===")
+    
 
+    
     ACOSXM().run()
